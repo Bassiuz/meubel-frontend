5b2d1ee21ea864675f7cefbde5aaf20845515306
Bassiuz <b@sdevaan.nl>
2019-08-07 22:19:42 +0200

    add new backend features

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,10 @@
         <h3>Welk meubel ben jij?</h3>
         <input v-model="naam" placeholder="vul in" />
         <button v-on:click="searchMeubel">naam</button>
-         <div v-for="meubel in randomMeubels" v-bind:key="meubel.name">
+        <button v-on:click="randomName">doe maar wat</button>
+         <div v-for="meubel in randomMeubels" v-bind:key="meubel.name" class="small-item">
             {{ meubel.name }}
-            <img :src="meubel.imageUrl" />
+            <img class='small-image' :src="meubel.imageUrl" />
          </div>
       </div>
       <div class="page">
@@ -21,15 +22,20 @@
           <h1 v-if="meubels !== null && meubels.length === 0">Helaas, {{submittedNaam}} is geen meubel</h1>
           <div v-for="meubel in meubels.slice(0,1)" v-bind:key="meubel.name">
               {{ meubel.name }}
-              <img :src="meubel.imageUrl"/>
+              <img class='big-image' :src="meubel.imageUrl"/><br/>
+              <button v-on:click="scoreMeubel(meubel.name)">cool, helemaal mee eens</button>
           </div>
       </div>
       <div class="page">
         <button v-on:click="again">Nog eens zoeken</button>
         <h3 v-if="meubels !== null && meubels.length > 0">Nee, {{submittedNaam}} is toch meer een: </h3>
-        <div v-for="meubel in meubels.slice(1)" v-bind:key="meubel.name">
-            {{ meubel.name }}
-            <img :src="meubel.imageUrl" />
+        <div v-for="meubel in meubels.slice(1)" v-bind:key="meubel.name" class="small-item">
+            <img class='small-image' :src="meubel.imageUrl" />
+
+            <div class="name-button-box">
+               {{ meubel.name }}
+              <button v-on:click="scoreMeubel(meubel.name)">nee, dit past beter</button>
+            </div>
          </div>
       </div>
     </div>
@@ -65,19 +71,39 @@ export default {
       } else {
         this.submittedNaam = this.naam
         // eslint-disable-next-line no-undef
-        console.log($('#flipbook'))
-        // eslint-disable-next-line no-undef
         $('#flipbook').turn(
           'next'
         )
-        axios
-          .get(
-            'https://meubel-backend.herokuapp.com/LeenBakker/Meubel?name=' +
-              this.naam
-          )
-          .then(response => (
-            this.meubels = response.data))
+        this.searchName(this.naam)
       }
+    },
+    searchName: function (name) {
+      axios.get(
+        'https://meubel-backend.herokuapp.com/Meubel/getAll?name=' + name)
+        .then(response => (this.meubels = response.data))
+    },
+    scoreMeubel: function (meubelName) {
+      axios.get(
+        'https://meubel-backend.herokuapp.com/Score/scoreByNameAndIndex?name=' + this.naam + '&meubelName=' + meubelName)
+        .then(this.again)
+    },
+    processRandomName: function (name) {
+      this.naam = name
+      this.submittedNaam = name
+      this.searchName(this.naam)
+    },
+    randomName: function (event) {
+      window.jQuery = require('jquery')
+      window.$ = window.jQuery
+      // eslint-disable-next-line no-undef
+      $('#flipbook').turn(
+        'next'
+      )
+      axios
+        .get(
+          'https://meubel-backend.herokuapp.com/Meubel/getRandomName'
+        )
+        .then(response => this.processRandomName(response.data))
     },
     again: function (event) {
       window.jQuery = require('jquery')
@@ -105,7 +131,7 @@ export default {
   },
   mounted () {
     axios
-      .get('https://meubel-backend.herokuapp.com/LeenBakker/Meubel?name=Bas')
+      .get('https://meubel-backend.herokuapp.com/Meubel/getTop5')
       .then(response => (this.randomMeubels = response.data))
     // eslint-disable-next-line no-undef
     $('#flipbook').turn({
@@ -164,5 +190,32 @@ div.flipbook
 {
   overflow: hidden;
   margin: 0 auto;
+}
+div.small-item
+{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1vh;
+}
+div.name-button-box
+{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+img.small-image
+{
+   width:100%;
+   max-width:8vw;
+   height:100%;
+   max-height:8vw;
+}
+img.big-image
+{
+   width:100%;
+   max-width:20vw;
+   height:100%;
+   max-height:20vw;
 }
 </style>
